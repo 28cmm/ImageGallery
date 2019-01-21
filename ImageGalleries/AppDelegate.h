956595fc0d8f0f4b538764762878056a71f381ba
28cmm <joshuafang1998@gmail.com>
2019-01-21 12:45:31 -0800

    Initial Commit

---
+++ ImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGalleries
+//
+//  Created by Yilei Huang on 2019-01-21.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
